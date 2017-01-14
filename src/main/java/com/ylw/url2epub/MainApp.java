56e8f20bf49818ed1d91eaede331c74038ba42f2
yuanliwei <891789592@qq.com>
2017-01-14 16:21:08 +0800

    Signed-off-by: yuanliwei <891789592@qq.com>

--- src/main/java/com/ylw/url2epub/MainApp.java
+++ src/main/java/com/ylw/url2epub/MainApp.java
@@ -4,8 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.sqlite.core.CoreConnection;
+
 import com.ylw.url2epub.model.UrlContent;
 import com.ylw.url2epub.net.Get;
+import com.ylw.url2epub.template.ContentOpf;
+import com.ylw.url2epub.template.ContentOpfGuide;
+import com.ylw.url2epub.template.ContentOpfManifest;
+import com.ylw.url2epub.template.ContentOpfMetadata;
+import com.ylw.url2epub.template.ContentOpfSpine;
+import com.ylw.url2epub.template.TocNcx;
+import com.ylw.url2epub.template.TocNcxDocTitle;
+import com.ylw.url2epub.template.TocNcxHead;
+import com.ylw.url2epub.template.TocNcxNavMap;
+import com.ylw.url2epub.utils.FileUtil;
 
 public class MainApp {
 
@@ -19,20 +31,40 @@ public class MainApp {
 		// TODO Auto-generated method stub
 		List<UrlContent> contents = new ArrayList<>();
 		AtomicInteger atomicInteger = new AtomicInteger(1);
-		Get.getContent(url, contents, atomicInteger, () -> {
+		new Get().getContent(url, contents, deep, atomicInteger, () -> {
 			generateEPUB(contents);
 		});
 
 	}
 
 	private void generateEPUB(List<UrlContent> contents) {
+		String rootDir = "C:\\Users\\y\\Desktop\\newEbook\\";
 		// content meta-data
 		// mainfest
 		// spine
 		// guide
 		// toc.ncx
-		//  head title navMap
-		
+		// head title navMap
+		String articleid = null;
+		String title = null;
+		String cover = null;
+		ContentOpfMetadata metadata = new ContentOpfMetadata(articleid, title, cover);
+		ContentOpfManifest manifest = new ContentOpfManifest(contents);
+		ContentOpfSpine spine = new ContentOpfSpine(contents);
+		String href = null;
+		ContentOpfGuide guide = new ContentOpfGuide(href, title);
+		ContentOpf contentOpf = new ContentOpf(metadata, manifest, spine, guide);
+		String contentOpfResult = contentOpf.parse();
+
+		FileUtil.saveFullPathFile(rootDir + "content.opt", contentOpfResult);
+
+		String uid = null;
+		TocNcxHead head = new TocNcxHead(cover, uid);
+		TocNcxDocTitle docTitle = new TocNcxDocTitle(title);
+		TocNcxNavMap navMap = new TocNcxNavMap(contents);
+		TocNcx tocNcx = new TocNcx(head, docTitle, navMap);
+		String tocNcxResult = tocNcx.parse();
+		FileUtil.saveFullPathFile(rootDir + "toc.ncx", tocNcxResult);
 
 	}
 }
--- src/main/java/com/ylw/url2epub/net/Get.java
+++ src/main/java/com/ylw/url2epub/net/Get.java
@@ -1,14 +1,36 @@
 package com.ylw.url2epub.net;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+
+import org.apache.http.HttpConnection;
+import org.junit.runner.Request;
 
 import com.ylw.url2epub.model.UrlContent;
 
 public class Get {
 
-	public static void getContent(String url, List<UrlContent> contents, AtomicInteger atomicInteger, CallBack callback) {
+	public void getContent(String url, List<UrlContent> contents, int deep, AtomicInteger atomicInteger,
+			CallBack callback) {
 		// TODO Auto-generated method stub
-		
+		if (deep == 0)
+			return;
+		get(url, (int code, String body) -> {
+
+		});
+	}
+
+	void get(String url, HTTPGETCallBack callBack) {
+		try {
+			URL url2 = new URL(url);
+			HttpConnection connection = (HttpConnection) url2.openConnection();
+		} catch (IOException e) {
+			e.printStackTrace();
+			callBack.onResult(-1, e.getMessage());
+		}
 	}
 }
